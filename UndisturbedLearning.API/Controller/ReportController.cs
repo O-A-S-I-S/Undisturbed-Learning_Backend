8573bd67b6b6cbd8b7a909290cc5d4371821568b
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-06-07 19:01:17 -0500

    Implementación del controlador-servicio de los reportes para la obtención del reporte de una cita en específico

---
+++ UndisturbedLearning.API/Controller/ReportController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UL_Testing.DataAccess;
+using UL_Testing.Dto.Request;
+using UL_Testing.Entities;
+
+namespace UL_Testing.API.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController:ControllerBase
+    {
+        private readonly UL_TestingDBContext _context;
+
+        public ReportController(UL_TestingDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<Report>>> Get()
+        {
+            ICollection<Report> response;
+
+            response = await _context.Reports.ToListAsync();
+
+
+            return Ok(response);
+
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Report>> Get(int id)
+        {
+            var entity = await _context.Reports.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("No se encontró el registro");
+            }
+
+            return Ok(entity);
+        }
+
+
+        [HttpPost("{id:int}")]
+        public async Task<ActionResult> Post(int id, DtoReport request)
+        {
+            var tempAppointment = await _context.Appointments.FindAsync(id);
+
+            if (tempAppointment == null) return NotFound();
+
+            var entity = new Report
+            {
+                Resolution = request.Resolution,
+                Brief = request.Brief,
+                Text = request.Text,
+                AppointmentId = tempAppointment.Id,
+            };
+
+            _context.Reports.Add(entity);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Response.Headers.Add("location", $"/api/report/{id}/{entity.Id}");
+
+            return Ok();
+        }
+
+    }
+}
---
+++ UndisturbedLearning.Dto/Request/DtoReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UL_Testing.Dto.Request
+{
+    public class DtoReport
+    {
+        public string Resolution { get; set; }
+        public string Brief { get; set; }
+        public string Text { get; set; }
+
+    }
+}

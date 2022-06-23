2d72b271a58db89a0490f34cabb7cae315c0238d
z3r0st <u202021767@upc.edu.pe>
2022-06-22 20:19:08 -0500

    migrations

---
+++ UndisturbedLearning.DataAccess/Migrations/20220606230234_RemoveExtraStudentWorkshopRel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UndisturbedLearning.DataAccess.Migrations
+{
+    public partial class RemoveExtraStudentWorkshopRel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentWorkshops");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentWorkshops",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    WorkshopId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentWorkshops", x => new { x.StudentId, x.WorkshopId });
+                    table.ForeignKey(
+                        name: "FK_StudentWorkshops_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentWorkshops_Workshops_WorkshopId",
+                        column: x => x.WorkshopId,
+                        principalTable: "Workshops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentWorkshops_WorkshopId",
+                table: "StudentWorkshops",
+                column: "WorkshopId");
+        }
+    }
+}
---
+++ UndisturbedLearning.DataAccess/Migrations/20220622183756_AddedActivityEntity.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UndisturbedLearning.DataAccess.Migrations
+{
+    public partial class AddedActivityEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Activity",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ActivityId",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cause",
+                table: "Appointments",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_ActivityId",
+                table: "Appointments",
+                column: "ActivityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Activities_ActivityId",
+                table: "Appointments",
+                column: "ActivityId",
+                principalTable: "Activities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Activities_ActivityId",
+                table: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_ActivityId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Cause",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Activity",
+                table: "Appointments",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
